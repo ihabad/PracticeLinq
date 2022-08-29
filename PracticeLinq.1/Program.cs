0e799dd4b714a3956153ea8c72d6f4a3fd583486
ihababoudiab@gmail.com <ihababoudiab@gmail.com>
2022-08-29 08:21:35 -0700

    initial commit

---
+++ PracticeLinq.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq._1
+{
+    internal class Program
+    {
+     
+        static void Main(string[] args)
+        {
+           List<string> namesOfGames = new List<string>()
+
+             {"League of Legends", "Thief II",  "Call of Duty", "Minecraft",};
+
+            namesOfGames.OrderBy(games => games.Length).ToList().ForEach(namesOfGames => Console.WriteLine(namesOfGames));
+
+            }
+
+        }
+    }
+
